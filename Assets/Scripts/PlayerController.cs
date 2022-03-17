65d3751d5b9e789407dbb1e3fb8e12f43617eedf
Layton4 <Javierete4774@gmail.com>
2022-03-17 21:33:06 +0100

    17 marzo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveForce = 1000;
-    public float rotateForce = 50;
+    //public float moveForce = 1000;
+    //public float rotateForce = 50;
     private float HorizontalInput;
     private float VerticalInput;
 
-    public float speed = 20;
-    private float turnspeed = 15;
+    public float speed = 4;
+    private float turnspeed = 50;
     private Rigidbody playerRigidbody;
 
     void Start()
@@ -27,8 +27,8 @@ public class PlayerController : MonoBehaviour
 
         //playerRigidbody.AddForce(Vector3.forward * speed * VerticalInput * moveForce);
         
-        transform.Rotate(Vector3.up * HorizontalInput * turnspeed * rotateForce* Time.deltaTime);
-        //transform.Translate(Vector3.forward * VerticalInput * speed * Time.deltaTime);
-        playerRigidbody.AddForce(gameObject.transform.forward * VerticalInput * moveForce);
+        transform.Rotate(Vector3.up * HorizontalInput * turnspeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * VerticalInput * speed * Time.deltaTime);
+        //playerRigidbody.AddForce(gameObject.transform.forward * VerticalInput * moveForce);
     }
 }
